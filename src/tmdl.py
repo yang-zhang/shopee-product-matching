3a1bc4edefea970294001cb8ec20500a064a829e
yang-zhang <zhangyangcolostate@gmail.com>
2021-03-21 02:20:01 +0000

    Add nbs

---
+++ src/tmdl.py
@@ -0,0 +1,57 @@
+from utils import *
+from transformers import AutoModel, AutoTokenizer
+
+BS = 256
+MAXLEN = 128
+
+
+def mk_tensors(txt, tokenizer, maxlen):
+    tok_res = tokenizer(txt, truncation=True, padding=True, max_length=maxlen)
+    input_ids = tok_res["input_ids"]
+    token_type_ids = tok_res["token_type_ids"]
+    attention_mask = tok_res["attention_mask"]
+    input_ids = torch.tensor(input_ids)
+    token_type_ids = torch.tensor(token_type_ids)
+    attention_mask = torch.tensor(attention_mask)
+    return input_ids, attention_mask, token_type_ids
+
+
+def mk_dl(tensors):
+    input_ids, token_type_ids, attention_mask = tensors
+    ds = TensorDataset(input_ids, attention_mask, token_type_ids)
+    dl = DataLoader(
+        dataset=ds,
+        batch_size=BS,
+        num_workers=NWKRS,
+        pin_memory=PIN_MEMORY,
+        shuffle=False,
+    )
+    return dl
+
+
+def mk_feats(df):
+    device = torch.device(DEVICE)
+    nm_mdl = "pvl/labse_bert"
+    tokenizer = AutoTokenizer.from_pretrained(nm_mdl, do_lower_case=False)
+    tensors = mk_tensors(list(df.title.values), tokenizer, maxlen=MAXLEN)
+    dl = mk_dl(tensors)
+    mdl = AutoModel.from_pretrained(nm_mdl).to(device)
+    mdl.eval()
+    feats = np.zeros((len(df), 768))
+    i = 0
+    for dat in dl:
+        with torch.no_grad():
+            dat = (o.to(device) for o in dat)
+            output = mdl(*dat)
+            fts = output.last_hidden_state
+            fts = fts.mean(dim=-2)
+            l = len(fts)
+            feats[i : i + l, :] = fts.cpu().detach().numpy()
+            i += l
+    return feats
+
+
+def mk_sims(df):
+    feats = mk_feats(df)
+    sims = cosine_similarity(feats)
+    return sims
---
+++ src/utils.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import numpy as np
+import torch
+from torch.utils.data import Dataset, TensorDataset, DataLoader
+
+import gc
+from sklearn.metrics.pairwise import cosine_similarity
+
+NWKRS = 8
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+PIN_MEMORY = True if torch.cuda.is_available() else False
+
+
+def getf1(x, y):
+    n = len(np.intersect1d(x, y))
+    return 2 * n / (len(x) + len(y))
+
+
+def getf1s(xs, ys):
+    return (getf1(x, y) for x, y in zip(xs, ys))
+
+
+def meanf1(xs, ys):
+    return np.mean(list(getf1s(xs, ys)))
+
+
+def mk_hsims(df):
+    hfeats = df.image_phash.apply(lambda x: bin(int(x, 16))).values
+    X = np.array([[eval(o) for o in x[2:]] for x in hfeats], dtype=np.bool)
+    n = len(X)
+    hdists = np.zeros((n, n), dtype=np.short)
+    for i in range(n):
+        hdists[:, i] = ((X[:, None, :] != X[i]).sum(2))[:, 0]
+    hsims = 1 / (1 + hdists)
+    return hsims
+
+
+def get_preds_by_thrsh(sims, thrsh, pids):
+    isclose = sims >= thrsh
+    preds_idx = (np.where(x)[0] for x in isclose)
+    return [pids[o] for o in preds_idx]
+
+
+# git/shopee-product-matching/working/dev0015--threshold--combine.ipynb
+thrsh_v = 0.81
+thrsh_t = 0.95
+thrsh_h = 0.11
